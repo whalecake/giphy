cdb95f6e231ac63b4b98f1da12f3502574507914
whalecake <ingrid.ang@codenation.org>
2020-03-17 05:15:29 +0000

    FINISHED project

--- js/script.js
+++ js/script.js
@@ -3,9 +3,38 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
+
 $("#search-button").click(function(){
-  
-  
-  
+    
+    var search = $("#search-term").val();
+    var newAPI = "https://api.giphy.com/v1/gifs/search?q=" + search + "&rating=pg&api_key=dc6zaTOxFJmzC";
+
+    $(".mainContainer").html(`<div class="row1 gallery"></div><div class="row2 gallery"></div><div class="row3 gallery"></div>`)
+
+    fetch (newAPI)
+
+    //asynchronisity: when the fetch request comes back, THEN this will happen
+    .then(function(response) {
+        return response.json();
+    })
+
+    .then(function(gifData){ 
+    
+        $("#introText").hide();
+    
+        for(i=0; i<4; i++) {
+            $(".row1").append(`<img class="rowOne" src=${gifData.data[i].images.original.url} height=200>`);
+        }
+
+        for(i=4; i<8; i++) {
+            $(".row2").append(`<img class="rowTwo" src=${gifData.data[i].images.original.url} height=200>`);
+        }
+
+        for(i=8; i<12; i++) {
+            $(".row3").append(`<img class="rowThree" src=${gifData.data[i].images.original.url} height=200>`);
+        }
+    
+  });
+
 });
 
